446e9ebf3c5890e9520475ef603cbdb73770bb0d
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-10-11 04:00:08 +0100

    Add Order and OrderItem models

---
+++ apps/orders/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+from apps.products.models import Product
+
+class Order(models.Model):
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
