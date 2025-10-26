dbda2a7fda9d8e64002d6b73cf5462e38a0eff94
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-10-26 21:42:18 +0000

    Update base.py

--- marketplace/settings/base.py
+++ marketplace/settings/base.py
@@ -74,3 +74,14 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django REST Framework configuration
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+}
