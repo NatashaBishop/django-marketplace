0fd20256cd863d5ecd0274e629006982f5cff809
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-12-19 03:30:12 +0000

    Create entrypoint.prod.sh

---
+++ production-ready -docker/entrypoint.prod.sh
@@ -0,0 +1,14 @@
+```ruby
+# bash 
+
+#!/bin/sh
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+exec gunicorn marketplace.wsgi:application --bind 0.0.0.0:8000 --workers 3
+```
