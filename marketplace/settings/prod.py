ade5e744299e06ad18515b45dddb0554cb22f188
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-12-22 15:19:06 +0000

    Modify production settings for ALLOWED_HOSTS
    Updated ALLOWED_HOSTS to use environment variable and added STATIC_ROOT and MEDIA_ROOT settings.

--- marketplace/settings/prod.py
+++ marketplace/settings/prod.py
@@ -1,4 +1,9 @@
+# pgsql
+
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["yourdomain.com"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
